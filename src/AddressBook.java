583a40e2df396efc4b4fb4228f3c9412467d4469
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-09-18 19:27:06 -0400

    Fix up address book to lab 3

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,49 +1,37 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddressBook {
-	private ArrayList<BuddyInfo> buddyList = new ArrayList<BuddyInfo>();
-	private String addressBookName;
+	private List<BuddyInfo> buddyInfo;
 	
 	/**
-	 * Obtain the list within the address book
-	 * @return
+	 * Class method to create AddressBook
 	 */
-	public ArrayList<BuddyInfo> getBuddyList() {
-		return buddyList;
+	public AddressBook() {
+		this.buddyInfo = new ArrayList<BuddyInfo>();
 	}
 	
 	/**
 	 * Add a new buddy into the address book
-	 * @param buddy, buddy to be added into the address book
+	 * @param aBuddyInfo, buddy to be added into the address book
 	 */
-	public void addBuddy(BuddyInfo buddy) {
-		buddyList.add(buddy);
+	public void addBuddy(BuddyInfo aBuddyInfo) {
+		if(aBuddyInfo != null) {
+			this.buddyInfo.add(aBuddyInfo);
+		}
 	}
-
+	
 	/**
 	 * Remove a buddy off the address book
 	 * @param index, index of buddy to be removed from the list
 	 */
-	public void removeBuddy(int index) {
-		buddyList.remove(index);
+	public BuddyInfo removeBuddy(int index) {
+		if(index >=0 && index < this.buddyInfo.size()) {
+			return this.buddyInfo.remove(index);
+		}
+		return null;
 	}
 
-	
-	/**
-	 * Get the name of this address book
-	 * @return addressBookName, return name of the address book
-	 */
-	public String getAddressBookName() {
-		return addressBookName;
-	}
-	
-	/**
-	 * Set a new name for the address book
-	 * @param addressBookName, the new name for the address book
-	 */
-	public void setAddressBookName(String addressBookName) {
-		this.addressBookName = addressBookName;
-	}
 
 	/**
 	 * Main function to run code
@@ -51,10 +39,7 @@ public class AddressBook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Address Book");
-		BuddyInfo buddy = new BuddyInfo();
-		buddy.setName("Micheal");
-		buddy.setAddress("Ottawa");
-		buddy.setPhoneNumber(66681788);
+		BuddyInfo buddy = new BuddyInfo("Micheal", "Ottawa", 66681788);
 		AddressBook addressBook = new AddressBook();
 		addressBook.addBuddy(buddy);
 		addressBook.removeBuddy(0);
